23b80a0fbd62b54c83ada48be387446b79a60a47
Nikhitha <nikhirdykun03@gmail.com>
2024-11-21 12:42:25 -0600

    Register

--- database.py
+++ database.py
@@ -9,3 +9,16 @@ def init_db():
     conn.commit()
     conn.close()
 
+def register_user(username,password):
+    hashed_password = generate_password_hash(password)
+    conn = sqlite3.connect('chat_app.db')
+    cursor = conn.cursor()
+    try:
+        cursor.execute("INSERT INTO users (username, password) VALUES (?,?)",(username, hashed_password))
+        conn.commit()
+        return True
+    except sqlite3.IntegrityError :
+        return False
+    finally:
+        conn.close()
+
