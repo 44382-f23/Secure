7a294d4931a477d67ab2ff6d774f9da3b7f3a1a1
Nikhitha <nikhirdykun03@gmail.com>
2024-12-01 12:51:02 -0600

    made changes wherever it is required

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 import sqlite3
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash,
 
 DATABASE = 'chat_app.db'
 
@@ -23,6 +23,7 @@ def register_user(username,password):
         return False   
     
     hashed_password = generate_password_hash(password)
+
     cursor.execute("INSERT INTO users (username, password) VALUES (?,?),(usernmae, hashed_password)")
 
     conn.commit()
@@ -30,7 +31,7 @@ def register_user(username,password):
     return True
 
 def get_user_password(username):
-    conn = sqlite3.connect('chat_app.db')
+    conn = sqlite3.connect(DATABASE)
     cursor = conn.cursor()
     cursor.execute("SELECT password FROM users WHERE username = ?", (username,))
     result = cursor.fetchone()
@@ -38,15 +39,17 @@ def get_user_password(username):
     return result[0] if result else None
 
 def save_message(username, message):
-    conn = sqlite3.connect('chat_app.db')
+    conn = sqlite3.connect(DATABASE)
     cursor = conn.cursor()
     cursor.execute("INSERT INTO messages(username, message) VALUES (?,?)",(username, message))
     conn.commit()
+    conn.close()
+
 
 def get_chat_history():
-    conn = sqlite3.connect('chat_app.db')
+    conn = sqlite3.connect(DATABASE)
     cursor = conn.cursor()
-    cursor.execute("SELECT username, message FROM messages ORDER BY id")
+    cursor.execute("SELECT username, message, timestamp FROM messages ORDER BY timestamp DESC")
     chat_history = cursor.fetchall()
     conn.close()
     return chat_history
