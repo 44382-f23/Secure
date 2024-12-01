ff48417709d6fd0ec5cf588cfdd0d498fe516446
Nikhitha <nikhirdykun03@gmail.com>
2024-12-01 11:52:56 -0600

    fixing error

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from werkzeug.security import generate_password_hash, check_password_hash
-import sqlite3
-from database import init_db, register_user, get_user_password, save_message, get_chat_history # type: ignore
+from database import init_db, register_user, get_user_password, save_message, get_chat_history 
 
 #Initialize the flask application
 app = Flask(__name__)
@@ -31,21 +30,25 @@ def login():
     return render_template('login.html')
 
 
+
+
 #Handling user registration
 @app.route('/register', methods = ['GET', 'POST'])
+
 def register():                                
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        username = request.form.get['username']
+        password = request.form.get['password']
 
         #Determines to get the user registration
-    if register_user(username, password):
-        flash("Registration successful! Please log in.")
-        return redirect(url_for('login'))
-    else:
-        flash("Username already exists.")
+        if register_user(username, password):
+            flash("Registration successful! Please log in.")
+            return redirect(url_for('login'))
+        else:
+            flash("Username already exists.")
     return render_template('register.html')
- 
+
+
 
 #Route for the chat functioning 
 @app.route('/chat', methods= ['GET', 'POST'])
